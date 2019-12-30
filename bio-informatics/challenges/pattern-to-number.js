c819036c3bd48763f831e8e99f9024f190a201a8
Nuno <30328808+storycoding@users.noreply.github.com>
2019-12-30 13:51:11 +0100

    Update: Completed patternToNumber algorythm

--- bio-informatics/challenges/pattern-to-number.js
+++ bio-informatics/challenges/pattern-to-number.js
@@ -1,3 +1,23 @@
-const patternToNumber = () => {};
+const patternToNumber = (pattern) => {
+    const genomeValues = {
+        "A" : 0,
+        "C" : 1,
+        "G" : 2,
+        "T" : 3
+    }
+    const quartary = Object.keys(genomeValues).length;
+
+    let number = 0;
+
+    for (let [i, digit] = [0, pattern.length - 1] ; i < pattern.length ; i++, digit--) {
+        const genome = pattern[i];
+        const genomeValue = genomeValues[genome];
+        const digitValue = quartary ** digit;
+        const iterationValue = genomeValue * digitValue;
+        number += iterationValue;
+    }
+
+    return number;
+};
 
 module.exports = patternToNumber;

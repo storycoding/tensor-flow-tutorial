ab2ccda9c4401f792458c5fb83f762159d12cbdb
Nuno <30328808+storycoding@users.noreply.github.com>
2019-12-30 12:19:05 +0100

    Update: Completed number-to-pattern

--- bio-informatics/challenges/number-to-pattern.js
+++ bio-informatics/challenges/number-to-pattern.js
@@ -1,3 +1,14 @@
-const numberToPattern = () => {};
+const numberToPattern = (number, patternLength) => {
+    const genomes = ["A", "C", "G", "T"];
+    let pattern = [];
+
+    for (let i = 0; i < patternLength; i++) {
+        const remainder = number % 4;
+        pattern.push(genomes[remainder]);
+        number = Math.floor(number / 4);
+    }
+
+    return pattern.reverse().join('');
+};
 
 module.exports = numberToPattern;
